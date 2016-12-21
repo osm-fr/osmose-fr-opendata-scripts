8c570fbe3f635dcd295e354ca4f86381cc25de43
cquest <cquest@osm13.openstreetmap.fr>
2016-12-21 21:52:08 +0100

    optimisation et mise en prod

--- analyzer-qa-missing-buildings.sh
+++ analyzer-qa-missing-buildings.sh
@@ -12,26 +12,22 @@ echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>
 
 psql osm -c "
 select format('<error class=\"50\" subclass=\"1\"><location lat=\"%s\" lon=\"%s\" /><relation id=\"%s\" /><text lang=\"fr\" value=\"(%s %s)\" /><text lang=\"en\" value=\"(%s %s)\" /></error>',
-	round(st_y(st_transform(st_pointonsurface(way),4326))::numeric,6),
-	round(st_x(st_transform(st_pointonsurface(way),4326))::numeric,6),
-	-p.osm_id,
-	insee, p.name,
-	insee, p.name)
-from (
-	select * from (
-		select	i.insee,
-			count(*)::numeric as total,
-			sum(case when buildings=0 then 1 else 0 end)::numeric as no_buildings
-		from insee_menages i
-		join cadastre c on (c.insee=i.insee)
-		where c.format='VECT' group by 1) as vecto
-	where no_buildings/total>0.5) as m
-	join planet_osm_polygon p on (p.tags ? 'ref:INSEE' and p.tags->'ref:INSEE'=m.insee and boundary='administrative' and admin_level='8');
+  round(st_y(st_transform(st_pointonsurface(way),4326))::numeric,6),
+  round(st_x(st_transform(st_pointonsurface(way),4326))::numeric,6),
+  -osm_id,
+  insee,
+  c.name,
+  insee,
+  c.name)
+from (select c.insee, count(i.*)::float as total, sum(case when i.buildings=0 then 1 else 0 end)::float as missing
+  from cadastre c join insee_menages i on (i.insee=c.insee)
+  where c.format='VECT' group by 1) as m
+join planet_osm_polygon c on (c.tags->'ref:INSEE'=m.insee) where c.tags ? 'ref:INSEE' and c.admin_level='8' and missing/total>0.5;
 " -t >> $OUT
 
 echo "
   </analyser>
 </analysers>" >> $OUT
 
-curl -s --request POST --form source='opendata_xref-france' --form code="$OSMOSEPASS" --form content=@$OUT http://dev.osmose.openstreetmap.fr/control/send-update
+curl -s --request POST --form source='opendata_xref-france' --form code="$OSMOSEPASS" --form content=@$OUT http://osmose.openstreetmap.fr/control/send-update
 #/usr/local/bin/http --timeout=300 --form POST http://dev.osmose.openstreetmap.fr/control/send-update source='opendata_xref-france' code="$OSMOSEPASS" content@$OUT
