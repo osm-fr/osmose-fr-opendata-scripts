a4c453abeefe535dfb53e1f26c7591439235cce8
marc-marc-marc <marc_marc_irc@hotmail.com>
2021-04-29 17:12:18 +0200

    lorsque le script est dans un sous-repertoire, dirname -bash donnera le sous-repertoire ce qui ne convient pas pour trouver le fichier mdp

--- config.sh
+++ config.sh
@@ -1,7 +1,11 @@
-if [ -f $(dirname ${0})/osmose_config_password.sh ]; then
-  echo "sourcing $(dirname ${0})/osmose_config_password.sh..."
-  . $(dirname ${0})/osmose_config_password.sh || exit 1
+#!/bin/bash
+#osmose_config_password=$(dirname ${0})/osmose_config_password.sh
+#lorsque le script est dans un sous-repertoire, dirname $0 donnera le sous-repertoire ce qui ne convient pas pour trouver le fichier mdp
+osmose_config_password=/home/cquest/osmose/osmose_config_password.sh
+if [ -f ${osmose_config_password} ]; then
+  echo "sourcing ${osmose_config_password}..."
+  . ${osmose_config_password} || exit 1
 else
-  echo "file $(dirname ${0})/osmose_config_password.sh not found. setting empty password"
+  echo "file ${osmose_config_password} not found. setting empty password"
   export OSMOSEPASS=""
 fi
