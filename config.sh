a398a4b5975110e52fcd8084e3dcf43cb4df0415
marc-marc-marc <marc_marc_irc@hotmail.com>
2023-01-03 00:06:03 +0100

    curl --max-time 1200 (20min)

--- config.sh
+++ config.sh
@@ -21,8 +21,8 @@ send_frontend() {
     until [ "$tries" -ge 3 ]; do
         tries=$(( $tries + 1 ))
         echo "Try: '$tries'"
-        curl -s --request POST --form source='opendata_xref-france' --form code="$OSMOSEPASS" --form content=@$OUT -H 'Host: osmose.openstreetmap.fr' "${URL_FRONTEND_UPDATE}" && echo curl ok && break
-	echo faild. sleeping 300 sec
+        curl -s --request POST --form source='opendata_xref-france' --form code="$OSMOSEPASS" --form content=@$OUT -H 'Host: osmose.openstreetmap.fr' --max-time 1200 "${URL_FRONTEND_UPDATE}" && echo curl ok && break
+	echo failed. sleeping 300 sec
         sleep 300
     done
 
